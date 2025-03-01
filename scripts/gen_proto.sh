3360bd6ff1fb2fd36d2ec6e1f8657a80257a6c47
khaivan <levankhai6729@gmail.com>
2025-03-02 02:45:57 +0700

    Update docker compose with booking service

--- scripts/gen_proto.sh
+++ scripts/gen_proto.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 protoc --go_out=api --go-grpc_out=api --proto_path=api/pricing api/pricing/*.proto
-protoc --go_out=api --go-grpc_out=api --proto_path=api/booking api/booking/*.proto
 protoc --go_out=api --go-grpc_out=api --proto_path=api/send api/send/*.proto
